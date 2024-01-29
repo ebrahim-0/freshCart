06acbe5f3cdabb9a47b9e2c2fd7a4cdf83a84f1d
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2024-01-29 02:45:49 +0200

    Update API endpoints

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,15 +10,13 @@ export class AuthService {
 
   register(userData: object): Observable<any> {
     return this._HttpClient.post(
-      'https://auth-64ql.onrender.com/api/auth/signup',
+      'http://localhost:5000/api/auth/signup',
       userData
     );
   }
 
   checkAuth(): Observable<any> {
-    return this._HttpClient.get(
-      'https://auth-64ql.onrender.com/api/auth/check-auth'
-    );
+    return this._HttpClient.get('http://localhost:5000/api/auth/check-auth');
   }
 
   // getCookies(): Observable<any> {
